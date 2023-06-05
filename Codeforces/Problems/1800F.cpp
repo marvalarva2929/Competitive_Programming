b28f883180951066b911cdf86ea74d22aaf9469e
marvalarva2929 <joshvigel329@outlook.com>
2023-06-04 21:23:44 -0500

    1800F 6/4

---
+++ Codeforces/Problems/1800F.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define int long long
+#define nl '\n'
+#define pi pair<int, int>
+#define ff first
+#define ss second
+
+using namespace std;
+const int mxn = 2e5;
+
+int a[mxn]; // amount
+int b[mxn]; // parity
+            
+unordered_map<int, int> cnt[26];
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(false);
+    
+    int n; cin >> n;
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        string s; cin >> s;
+        for (int j = 0; j < s.size(); j++) {
+            int bit = s[j] - 'a';
+            a[i] |= (1 << bit);
+            b[i] ^= (1 << bit);
+        }
+
+        for (int k = 0; k < 26; k++)
+            if (!((a[i] >> k) & 1)) 
+                    cnt[k][b[i]]++;
+    }
+
+    //for (int i = 0; i < 26; i++) {
+    //   cout << i << " : " << nl;
+    //   for (auto &p : cnt[i])
+    //       cout << bitset<26>(p.ff).to_string() << " " << p.ss << nl;
+    //} 
+        
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 26; j++)
+            if (!((a[i] >> j) & 1)) 
+                ans += cnt[j][((b[i] ^ ((1 << 26) - 1)) - (1 << j))];
+    }
+    cout << ans/2 << nl;
+}
+
+
